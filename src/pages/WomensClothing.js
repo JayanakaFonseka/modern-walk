8cb875f811a7bdb476ca5743453ecee94cfdac00
Jayanaka Fonseka <jayanakafonseka@gmail.com>
2024-04-24 13:22:39 +0530

    Removed unwanted code lines

--- src/pages/WomensClothing.js
+++ src/pages/WomensClothing.js
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Card from "../components/Card/Card";
 import Loader from "../components/Loader/Loader";
 import CardContext from "../context/cardContext";
